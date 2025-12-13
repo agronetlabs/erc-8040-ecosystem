adc4b320bdbbe8232100abc9034bc59bdb9c42fd
agronetlabs <leandro@agronet.io>
2025-12-13 03:50:06 -0300

    Add erc8040.hpp main header

---
+++ cpp-sdk/include/erc8040/erc8040.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "esg.hpp"
+#include "compliance.hpp"
+#include "iso20022.hpp"
+
+namespace erc8040 {
+    constexpr const char* VERSION = "0.1.0";
+    constexpr const char* STANDARD_ID = "ERC-8040";
+}
